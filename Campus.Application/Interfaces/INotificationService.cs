cb0f049139acbb57e26ec927529c626663df1578
vandriyv <andrvoitovych@gmail.com>
2019-11-10 15:22:32 +0200

    add exception and notificationservice interface

---
+++ Campus.Application/Interfaces/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Campus.Application.Notifications.Models;
+
+namespace Campus.Application.Interfaces
+{
+    public interface INotificationService
+    {
+        Task SendMessageAsync(Message message);
+    }
+}
---
+++ Campus.Application/Notifications/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Campus.Application.Notifications.Models
+{
+    public class Message
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
