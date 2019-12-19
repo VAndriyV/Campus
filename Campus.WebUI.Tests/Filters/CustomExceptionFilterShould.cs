0afb4bfacea650a7abd10f82154160308aee3353
vandriyv <andrvoitovych@gmail.com>
2019-12-19 22:51:12 +0200

    customexceptionfiltertests

--- Campus.WebUI.Tests/Filters/CustomExceptionFilterShould.cs
+++ Campus.WebUI.Tests/Filters/CustomExceptionFilterShould.cs
@@ -1,7 +1,8 @@
-﻿using NUnit.Framework;
+﻿using Campus.Application.Exceptions;
+using Campus.WebUI.Filters;
+using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Campus.WebUI.Tests.Filters
@@ -9,10 +10,79 @@ namespace Campus.WebUI.Tests.Filters
     [TestFixture]
     public class CustomExceptionFilterShould
     {
+        protected CustomExceptionFilterAttribute CustomExceptionFilterAttribute;
+
+        [SetUp]
+        protected void Init()
+        {
+            CustomExceptionFilterAttribute = new CustomExceptionFilterAttribute();
+        }
+
         [Test]
         public async Task Set_404_Response_StatusCode_When_NotFoundException_Occurs()
         {
-            
+            var notFoundException = new NotFoundException("Entity name");
+            var exceptionContext = CustomExceptionFilterTestHelper.GetExceptionContext(notFoundException);
+
+            CustomExceptionFilterAttribute.OnException(exceptionContext);
+
+            Assert.AreEqual((int)HttpStatusCode.NotFound, exceptionContext.HttpContext.Response.StatusCode);
         }
+
+        [Test]
+        public async Task Set_400_Response_StatusCode_When_ValidationException_Occurs()
+        {
+            var validationException = new ValidationException();
+            var exceptionContext = CustomExceptionFilterTestHelper.GetExceptionContext(validationException);
+
+            CustomExceptionFilterAttribute.OnException(exceptionContext);
+
+            Assert.AreEqual((int)HttpStatusCode.BadRequest, exceptionContext.HttpContext.Response.StatusCode);
+        }
+
+        [Test]
+        public async Task Set_409_Response_StatusCode_When_DuplicateException_Occurs()
+        {
+            var duplicateException = new DuplicateException("Entity name");
+            var exceptionContext = CustomExceptionFilterTestHelper.GetExceptionContext(duplicateException);
+
+            CustomExceptionFilterAttribute.OnException(exceptionContext);
+
+            Assert.AreEqual((int)HttpStatusCode.Conflict, exceptionContext.HttpContext.Response.StatusCode);
+        }
+
+        [Test]
+        public async Task Set_409_Response_StatusCode_When_DeleteFailureException_Occurs()
+        {
+            var deleteFailureException = new DeleteFailureException("Entity name", "key", "message");
+            var exceptionContext = CustomExceptionFilterTestHelper.GetExceptionContext(deleteFailureException);
+
+            CustomExceptionFilterAttribute.OnException(exceptionContext);
+
+            Assert.AreEqual((int)HttpStatusCode.Conflict, exceptionContext.HttpContext.Response.StatusCode);
+        }
+
+        [Test]
+        public async Task Set_401_Response_StatusCode_When_InvalidPasswordException_Occurs()
+        {
+            var invalidPasswordException = new InvalidPasswordException();
+            var exceptionContext = CustomExceptionFilterTestHelper.GetExceptionContext(invalidPasswordException);
+
+            CustomExceptionFilterAttribute.OnException(exceptionContext);
+
+            Assert.AreEqual((int)HttpStatusCode.Unauthorized, exceptionContext.HttpContext.Response.StatusCode);
+        }
+
+        [Test]
+        public async Task Set_500_Response_StatusCode_When_Unexpected_Occurs()
+        {
+            var exception = new Exception();
+            var exceptionContext = CustomExceptionFilterTestHelper.GetExceptionContext(exception);
+
+            CustomExceptionFilterAttribute.OnException(exceptionContext);
+
+            Assert.AreEqual((int)HttpStatusCode.InternalServerError, exceptionContext.HttpContext.Response.StatusCode);
+        }
+
     }
 }
---
+++ Campus.WebUI.Tests/Filters/CustomExceptionFilterTestHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+
+namespace Campus.WebUI.Tests.Filters
+{
+    static class CustomExceptionFilterTestHelper
+    {
+        public static ExceptionContext GetExceptionContext(Exception exception)
+        {
+            var httpContext = new DefaultHttpContext();
+            var actionContext = new ActionContext
+            {
+                HttpContext = httpContext,
+                RouteData = new RouteData(),
+                ActionDescriptor = new ActionDescriptor()
+            };
+
+            var exceptionContext = new ExceptionContext(actionContext,new List<IFilterMetadata>())                
+            {
+                Exception = exception
+            };
+
+            return exceptionContext;
+        }
+    }
+}
